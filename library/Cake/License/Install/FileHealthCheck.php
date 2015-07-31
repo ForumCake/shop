38a0685415e456cf8b9e5c4806b63cf2c18dacd5
Jonathan Wainwright <jon@waindigo.org>
2015-07-31 09:07:24 +0100

    File health check and tidy up XML files

---
+++ library/Cake/License/Install/FileHealthCheck.php
@@ -0,0 +1,14 @@
+<?php
+namespace Cake\License;
+
+class Install_FileHealthCheck extends \Cake\Install_FileHealthCheckBase
+{
+    
+    public function getFileHashes()
+    {
+        return array(
+        	'library/Cake/License/Install/Controller.php' => 'ca8cbf75ef6bd34f2bd9688852681754',
+        	'library/Cake/License/Install/Data.php' => 'd253aa7fd735e76c767c729f0ea4c183',
+        );
+    }
+}
