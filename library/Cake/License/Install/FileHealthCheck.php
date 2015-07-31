3e3f2e16bf5328b9e85b02e22e35ddaac059eaca
Jonathan Wainwright <jon@waindigo.org>
2015-07-31 09:21:42 +0100

    Add xml files to file health check

--- library/Cake/License/Install/FileHealthCheck.php
+++ library/Cake/License/Install/FileHealthCheck.php
@@ -7,6 +7,7 @@ class Install_FileHealthCheck extends \Cake\Install_FileHealthCheckBase
     public function getFileHashes()
     {
         return array(
+        	'library/Cake/License/addon-Cake_License.xml' => 'fba70e3dbe0fe4df6a227730929e613a',
         	'library/Cake/License/Install/Controller.php' => 'ca8cbf75ef6bd34f2bd9688852681754',
         	'library/Cake/License/Install/Data.php' => 'd253aa7fd735e76c767c729f0ea4c183',
         );
