f1d4dad4b71c3ea6107fc55fb1e057b3bab182d9
Jonathan Wainwright <jon@waindigo.org>
2015-08-03 11:09:20 +0100

    Add License module

--- library/Cake/License/Install/FileHealthCheck.php
+++ library/Cake/License/Install/FileHealthCheck.php
@@ -7,7 +7,7 @@ class Install_FileHealthCheck extends \Cake\Install_FileHealthCheckBase
     public function getFileHashes()
     {
         return array(
-        	'library/Cake/License/addon-Cake_License.xml' => 'df00e6fe3fe2b73bbf59364dba6809f5',
+        	'library/Cake/License/addon-Cake_License.xml' => '99251a640e88bf4863b798602068e9ee',
         	'library/Cake/License/Install/Controller.php' => 'ca8cbf75ef6bd34f2bd9688852681754',
         	'library/Cake/License/Install/Data.php' => 'd253aa7fd735e76c767c729f0ea4c183',
         );
