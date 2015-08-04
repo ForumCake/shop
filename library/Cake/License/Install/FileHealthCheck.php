110581bd6f6216e6e7f119772afa95c8dea49986
Jonathan Wainwright <jon@waindigo.org>
2015-08-04 10:01:24 +0100

    Add License module and move cake_license_details phrase to Extras

--- library/Cake/License/Install/FileHealthCheck.php
+++ library/Cake/License/Install/FileHealthCheck.php
@@ -7,7 +7,7 @@ class Install_FileHealthCheck extends \Cake\Install_FileHealthCheckBase
     public function getFileHashes()
     {
         return array(
-        	'library/Cake/License/addon-Cake_License.xml' => '99251a640e88bf4863b798602068e9ee',
+        	'library/Cake/License/addon-Cake_License.xml' => 'a415386c8b6f3a7efc1ef24c591d5deb',
         	'library/Cake/License/Install/Controller.php' => 'ca8cbf75ef6bd34f2bd9688852681754',
         	'library/Cake/License/Install/Data.php' => 'd253aa7fd735e76c767c729f0ea4c183',
         );
