00b9783879fbe5ef0ef18aae01e4c28e2fde4b4b
Jonathan Wainwright <jon@waindigo.org>
2015-08-07 12:53:25 +0100

    Alpha 9

--- library/Cake/Shop/Install/FileHealthCheck.php
+++ library/Cake/Shop/Install/FileHealthCheck.php
@@ -7,7 +7,7 @@ class Install_FileHealthCheck extends \Cake\Install_FileHealthCheckBase
     public function getFileHashes()
     {
         return array(
-        	'library/Cake/Shop/addon-Cake_Shop.xml' => '3f4e82412cf2d93f5f8dc0fcf29895db',
+        	'library/Cake/Shop/addon-Cake_Shop.xml' => '0f7b393d32d009b23ff27625bb5d9000',
         	'library/Cake/Shop/Install/Controller.php' => 'dad1dfb2e7400ef1562a8d3b0f0b4e5c',
         	'library/Cake/Shop/Install/Data.php' => 'd6908703b5e99fe1cbc698638db200a6',
         	'js/cake/shop/index.html' => 'd41d8cd98f00b204e9800998ecf8427e',
